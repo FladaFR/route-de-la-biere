6b956f64f03302a2f98e616561067945a33f1d9b
FladaFR <federicojreyes@outlook.com>
2026-04-16 14:43:21 +0200

    add supabase connection

--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,5 @@ export default async function Home() {
   const { data, error } = await supabase.from('test').select('*')
 
   if (error) return <p>Connection failed: {error.message}</p>
-  return <p>Supabase connected! 🎉</p>
+  return <p>Bienvenue à l'app de La Route de la Biere! 🎉</p>
 }
